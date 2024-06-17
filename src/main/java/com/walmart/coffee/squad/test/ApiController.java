c1016e9afb0ecbc6d4ecc9d53e01e653c557f45b
r0g07a6 <ruchik.gabhawala@walmart.com>
2024-06-16 17:51:56 -0700

    Adding object mapper

--- src/main/java/com/walmart/coffee/squad/test/ApiController.java
+++ src/main/java/com/walmart/coffee/squad/test/ApiController.java
@@ -1,9 +1,12 @@
 package com.walmart.coffee.squad.test;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
 
 import java.io.IOException;
 import java.util.Map;
@@ -15,8 +18,10 @@ public class ApiController {
 
     @PostMapping(value = "/v1/data", consumes = MediaType.APPLICATION_JSON_VALUE,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Object> addPickupPointService(@RequestBody Object requestData) {
-        System.out.println(requestData.toString());
+    public ResponseEntity<Object> addPickupPointService(@RequestBody Object requestData) throws JsonProcessingException {
+        ObjectWriter ow = new ObjectMapper().writer().withDefaultPrettyPrinter();
+        String json = ow.writeValueAsString(requestData);
+        System.out.println(json);
         return ResponseEntity.ok(requestData);
     }
 }
