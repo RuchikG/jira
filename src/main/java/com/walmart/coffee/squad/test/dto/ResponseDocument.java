cb5b6c024530860f20d9cb4fe0a386465e7bd00e
y0g0436 <yash.ghatole@walmart.com>
2024-06-18 18:44:14 -0700

    SCTD-1234 Scheduler and Get data from ES

--- src/main/java/com/walmart/coffee/squad/test/dto/ResponseDocument.java
+++ src/main/java/com/walmart/coffee/squad/test/dto/ResponseDocument.java
@@ -9,10 +9,7 @@ import java.util.List;
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ResponseDocument implements Serializable {
-    /*
-    Description: ResponseDocument is for multiple github events retrieved by singleId should be mapped to ResponseDocument
-    TODO: Check ResponseDTO fields
-     */
+
     private static final long serialVersionUID = 1L;
 
     String type;
